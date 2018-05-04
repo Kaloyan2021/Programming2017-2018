df0a6c5ed94a418a9604cde051a29f5c52b21f38
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-05-04 15:31:23 +0300

    PythonClassesBoardDrawing

--- PROGRAMMING/Python/8quenns.py
+++ PROGRAMMING/Python/8quenns.py
@@ -3,6 +3,32 @@
 #1| q |   |   |   |
 #2|   |   |   | q |
 #3|   | q |   |   |
+import turtle
+t = turtle.Pen()
+def chessBoard(x):
+    a = (x*50)//2
+    t.penup()
+    t.goto(-a,-a)
+    t.pendown()
+    t.speed(100)
+    z = -a + 50
+    for j in range(x):
+        for i in range(x):
+            t.forward(50)
+            for j in range(4):
+                t.left(90)
+                t.forward(50)
+        t.penup()
+        t.goto(-a,z)
+        t.pendown()
+        z += 50
 class Board():
-ednomerni masivi, horizontal, vertikal + abs(point(now.x) + point(now.x+1) != point(now.y) + point(now.y + 1))
-
+    def posibility(self):
+        return 1
+    def draw(self):
+        if possibility:
+            chessBoard((int(input("Input table")))
+#chessBoard(int(input("Input table")))
+a = Board()
+d.draw()
+input()
