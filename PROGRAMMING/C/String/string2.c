c82d552032704c5f223e50f12bda8ce7c50f7def
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-05-09 18:55:46 +0300

    string2.c

--- PROGRAMMING/C/String/string2.c
+++ PROGRAMMING/C/String/string2.c
@@ -3,13 +3,17 @@
 #include <string.h>
 
 int main(){
-    char names[1000][21];
+    char names[1000][21], s[100];
     int n = 0;
-    printf("\nInput student number -> ");
-    scanf("%d",&n);
-    for (int i = 0; i < n; i++) scanf("%s\n", &names[i]);
-    //for (int j = 0; j < n; j++) printf("%s\n", names[j]);
-
+    //printf("\nInput student number -> ");
+    gets(s);
+    //scanf("%d",&n);
+    //for (int i = 0; i < n; i++) scanf("%s\n", &names[i]);
+    //for (int j = 0; j <= n + 1; j++) printf("%s\n", names[j]);
+    for (int i = 0; s[i] != 0; i++){
+        if (s[i] == ' ') n++;
+    }
+    printf("%d",n);
 
 
     return 0;
