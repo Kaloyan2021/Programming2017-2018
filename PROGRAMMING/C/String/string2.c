ace3172d8c07935b2ce45265613461c977f5b4e7
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-05-11 10:33:27 +0300

    Strings2.c - done : 1. how many times ' ' is in string1 and how many times string2 is in string1

--- PROGRAMMING/C/String/string2.c
+++ PROGRAMMING/C/String/string2.c
@@ -3,18 +3,24 @@
 #include <string.h>
 
 int main(){
-    char names[1000][21], s[100];
-    int n = 0;
+    char names[1000][21], s[100], s2[100];
+    int n = 0, ok, a=0;
     //printf("\nInput student number -> ");
     gets(s);
-    //scanf("%d",&n);
-    //for (int i = 0; i < n; i++) scanf("%s\n", &names[i]);
-    //for (int j = 0; j <= n + 1; j++) printf("%s\n", names[j]);
+    gets(s2);
     for (int i = 0; s[i] != 0; i++){
         if (s[i] == ' ') n++;
     }
-    printf("%d",n);
-
-
+    printf("\n Intervals %d",n);
+    if(strlen(s) > strlen(s2)){
+        for (int i = 0; s[i] != 0; i++){
+            ok = 1;
+            for (int j = 0; s2[j] != 0; j++){
+                if (s[i+j] != s2[j]) ok = 0;
+            }
+            if(ok) a++;
+        }
+    }
+    printf("\n How many times s2 in s -> %d", a);
     return 0;
 }
