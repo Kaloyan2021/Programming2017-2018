acdce5a7c3ad9012df3432a578767d5d9c65e783
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-05-12 12:08:52 +0300

    Ax+B=0 -> string3.c

--- PROGRAMMING/C/String/string3.c
+++ PROGRAMMING/C/String/string3.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 
-int main(){//ax+b=0, a and b inputed from client in one line of string, summed N 
-
+int main(){//ax+b=0, a and b inputed from client in one line of string, summed N
+    int j, n;
+    char *p;
+    float  a, b, x = 0.0;
+    char strin[1000][100], hsrin[100], ret;
+    scanf("%d",&n);
+    getchar();
+    for (int i = 0; i < n; i++) gets(strin[i]);
+    for (int k = 0; k < n; k++)
+    {
+        p=strstr(strin[k],"a=");
+        if(p) a=atof(p+2);
+        p=strstr(strin[k],"b=");
+        if(p) b=atof(p+2);
+        if(a == 0) printf ("\nEvery X is solution");
+        else if (b == 0) printf("\nX = %lf",x);
+        else{ 
+            x = (-b)/a;
+            printf("\nX = %lf", x);
+        }
+        
+        //printf("\nA = %f; B = %f",a,b);
+    }
 
 
     return 0;
