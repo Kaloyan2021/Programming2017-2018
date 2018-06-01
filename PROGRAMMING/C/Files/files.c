b7b06187d43b0c554f0d5286585fe62cbb63be40
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-06-01 23:32:15 +0300

    file.c

--- PROGRAMMING/C/Files/files.c
+++ PROGRAMMING/C/Files/files.c
@@ -9,7 +9,7 @@ FILE *myFileAdd;
 void myWrite(char *);
 void myRead(char *);
 void myAdd(char *);
-
+void myOpt(int *op);
 
 
 void main(){
@@ -22,7 +22,143 @@ void main(){
     do{
         system("cls");
         printf("\n\t\t\tUse Up/Down Arrow to Begin");
-        if (op == 1)
+        myOpt(op);
+        c = getch();
+        if(c == 0 || c == 224)
+        {
+            c = getch();
+            if(c == 80)
+            {
+                if(op < 6) op++;
+                else op = 1;
+            }
+            else if(c == 72)
+            {
+                if(op > 1) op--;
+                else op = 6;
+            }
+        }else if(c == 13){
+            choice = op;
+            switch(choice)
+            {
+
+                case 1:
+                    system("cls");
+                    myWrite(filename);
+                    break;
+
+                case 2:
+                    system("cls");
+                    myRead(filename);
+                    break;
+
+                case 3:
+                    system("cls");
+                    myAdd(filename);
+                    break;
+
+                case 4:
+                    system("cls");
+                    printf("\nChoose a Path to your file");
+                    gets(filename);
+                    break;
+
+                case 5:
+                    exit(1);
+
+                case 6:
+                    system("shutdown /s");
+                    exit(1);
+            }
+        }
+
+    }while(1 /*choice != 5*/);
+}
+
+void myRead(char *filename)
+{
+    char s[100];
+    myFileRead = fopen(filename,"r");
+    if(myFileRead)
+    {
+        printf("\nYou wished to read from file %s", filename);
+        while(!feof(myFileRead))
+        {
+            if(fgets(s,99,myFileRead))
+                printf("\n%s",s);
+        }
+
+        fclose(myFileRead);
+    }else perror("ERROR in oppening file");
+    getch();
+}
+void myWrite(char *filename)
+{
+    char s[100];
+    myFileWrite = fopen(filename,"w");
+    if(myFileWrite)
+    {
+        printf("Write what you wish to write, to end write 'end', 'END' or something in between or simply");
+        printf("\n");
+        do
+        {
+            gets(s);
+            if(strstr(s,"END"))break;
+            if(strstr(s,"end"))break;
+
+            if(strstr(s,"End"))break;
+            if(strstr(s,"ENd"))break;
+            if(strstr(s,"EnD"))break;
+
+            if(strstr(s,"eNd"))break;
+            if(strstr(s,"ENd"))break;
+            if(strstr(s,"eND"))break;
+
+            if(strstr(s,"enD"))break;
+            if(strstr(s,"nND"))break;
+            if(strstr(s,"EnD"))break;
+
+            fprintf(myFileWrite,"%s\n",s);
+        }while(1);
+        fclose(myFileWrite);
+    }else perror("ERROR in oppening file");
+}
+void myAdd(char *filename)
+{
+    char s[100];
+    myFileAdd = fopen(filename,"r+"); // it can be also with a+
+    if(myFileAdd)
+    {
+        system("cls");
+        myRead(filename);
+        do
+        {
+            gets(s);
+            if(strstr(s,"END"))break;
+            if(strstr(s,"end"))break;
+
+            if(strstr(s,"End"))break;
+            if(strstr(s,"ENd"))break;
+            if(strstr(s,"EnD"))break;
+
+            if(strstr(s,"eNd"))break;
+            if(strstr(s,"ENd"))break;
+            if(strstr(s,"eND"))break;
+
+            if(strstr(s,"enD"))break;
+            if(strstr(s,"nND"))break;
+            if(strstr(s,"EnD"))break;
+
+            fprintf(myFileAdd,"%s\n",s);
+        }while(1);
+        //}
+        fclose(myFileAdd);
+    }else perror("ERROR in oppening file");
+}
+
+void myOpt(int *op)
+{
+    if (op == 1)
         {
             printf("\n > 1.Write");
             printf("\n   2.Read");
@@ -76,140 +212,4 @@ void main(){
             printf("\n   5.EXIT");
             printf("\n > 6.Shutdown the computer");
         }
-
-        c = getch();
-        if(c == 0 || c == 224)
-        {
-            c = getch();
-            if(c == 80)
-            {
-                if(op < 6) op++;
-                else op = 1;
-            }
-            else if(c == 72)
-            {
-                if(op > 1) op--;
-                else op = 6;
-            }
-        }else if(c == 13){
-            choice = op;
-            switch(choice)
-            {
-
-                case 1:
-                    system("cls");
-                    myWrite(filename);
-                    break;
-
-                case 2:
-                    system("cls");
-                    myRead(filename);
-                    break;
-
-                case 3:
-                    myFileAdd = fopen(filename,"r+");
-                    if(myFileAdd)
-                    {
-                        system("cls");
-                        while(!feof(myFileAdd))
-                        {
-                            if(fgets(s,99,myFileAdd))
-                                printf("\n%s",s);
-                        }
-                        printf("\nYou can Add in %s, write 'end', 'END' or something in between to end writing: \n");
-                        do
-                        {
-                            gets(s);
-                            if(strstr(s,"END"))break;
-                            if(strstr(s,"end"))break;
-
-                            if(strstr(s,"End"))break;
-                            if(strstr(s,"ENd"))break;
-                            if(strstr(s,"EnD"))break;
-
-                            if(strstr(s,"eNd"))break;
-                            if(strstr(s,"ENd"))break;
-                            if(strstr(s,"eND"))break;
-
-                            if(strstr(s,"enD"))break;
-                            if(strstr(s,"nND"))break;
-                            if(strstr(s,"EnD"))break;
-
-                            fprintf(myFileAdd,"%s\n",s);
-                        }while(1);
-                        //}
-                        fclose(myFileAdd);
-                    }else perror("ERROR in oppening file");
-                    break;
-
-                case 4:
-                    system("cls");
-                    printf("\nChoose a Path to your file");
-                    gets(filename);
-                    break;
-
-                case 5:
-                    exit(1);
-                case 6:
-                    system("shutdown /s");
-                    exit(1);
-            }
-        }
-
-    }while(1 /*choice != 5*/);
-}
-
-void myRead(char *filename)
-{
-    char s[100];
-    myFileRead = fopen(filename,"r");
-    if(myFileRead)
-    {
-        printf("\nYou wished to read from file %s", filename);
-        if(!feof(myFileRead)) printf("Nothing in this file, Sorry, try something else");
-        while(!feof(myFileRead))
-        {
-            if(fgets(s,99,myFileRead))
-                printf("\n%s",s);
-        }
-
-        fclose(myFileRead);
-    }perror("ERROR in oppening file");
-    getch();
-}
-void myWrite(char *filename)
-{
-    char s[100];
-    myFileWrite = fopen(filename,"w");
-    if(myFileWrite)
-    {
-        printf("Write what you wish to write, to end write 'end', 'END' or something in between or simply");
-        printf("\n");
-        do
-        {
-            gets(s);
-            if(strstr(s,"END"))break;
-            if(strstr(s,"end"))break;
-
-            if(strstr(s,"End"))break;
-            if(strstr(s,"ENd"))break;
-            if(strstr(s,"EnD"))break;
-
-            if(strstr(s,"eNd"))break;
-            if(strstr(s,"ENd"))break;
-            if(strstr(s,"eND"))break;
-
-            if(strstr(s,"enD"))break;
-            if(strstr(s,"nND"))break;
-            if(strstr(s,"EnD"))break;
-
-            fprintf(myFileWrite,"%s\n",s);
-        }while(1);
-        fclose(myFileWrite);
-    }perror("ERROR in oppening file");
-}
-void myAdd(char *filename)
-{
-    char s[100];
-    
 }
