14fa1855e5ae50c3e3275dac8d565c864de6286c
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2018-07-13 18:53:24 +0300

    Everything is finished, except outputing

--- PROGRAMMING/C/Files/MarksBook.c
+++ PROGRAMMING/C/Files/MarksBook.c
@@ -11,7 +11,7 @@ FILE *myFileR, *myFileW;
 void opt(int);
 
 typedef struct{
-    char name[100], subj[maxSubj][100], teacher[maxSubj][100], class[2];
+    char name[100], subj[maxSubj][100], teacher[maxSubj][100], class[1];
     int marks[maxSubj][maxMrks], grade, num, money;
     float avrAll, avrEach[maxSubj][100];
 }marksBook;
@@ -19,49 +19,54 @@ typedef struct{
 void main()
 {
     marksBook myBook;
-    int i = 0, j = 0, z = 0, k++, mark;
-    while(myBook.subj[i] != 0){
+    int i = 0, j = 0, z = 0, k = 0, mark;
+    while(!(strstr(myBook.subj[i],"done"))){
         printf("\nAsking for name -");
-        
+            gets(myBook.name);
         printf("\nAsking for grade - ");
-        
+            scanf("%d",&myBook.grade);
         printf("\nAsking for class - ");
-        
+            scanf("%c",&myBook.class);
         printf("\nAsking for number - ");
-        
+            scanf("%d",&myBook.num);
         printf("\nAsking for subject - ");
-        
+            gets(myBook.subj[i]);
         printf("\nAsking for teacher - ");
-        
+            gets(myBook.teacher[i]);
         j = 0;
         while(myBook.marks[i][j] != 0){
             printf("\nAsking for mark - ");
             scanf("%d",myBook.marks[i][j]);
             myBook.avrEach[i][0] += myBook.marks[i][j];
             myBook.avrAll += myBook.marks[i][j];
-            j++;
-            z++;
             if(myBook.marks[i][j] == 6) myBook.money += 8;
             else if(myBook.marks[i][j] == 5) myBook.money += 4;
             else if(myBook.marks[i][j] == 3) myBook.money -= 4;
             else if(myBook.marks[i][j] == 2) myBook.money -= 25;
             else myBook.money += 0;
             if(myBook.marks[i][j] < 5) k++;
+            j++;
+            z++;
         }
-        if (k < 0) printf("You can make it better with %s", myBook.subj[i]);
+        if (k > 0) printf("\nYou can make it better with %s", myBook.subj[i]);
+        else printf("\nYou're fine for now, keep the good work up");
         myBook.avrEach[i][0] /= j+1;
         i++;
     }
     myBook.avrAll /= z;
     if(myBook.avrAll > 5.5) printf("\nHave scholarship");
+    else printf("\nYou don't have scholarship");
+
+
+
 }
 
+/*
 void opt(int choice){
     if(choice == 1) printf("\n > 1.New\n   2.Add\n   3.Exit");
     else if(choice == 2) printf("\n   1.New\n > 2.Add\n   3.Exit");
     else printf("\n   1.New\n   2.Add\n > 3.Exit");
 }
-/*
 switch(choice){
     case 1:
         //New
